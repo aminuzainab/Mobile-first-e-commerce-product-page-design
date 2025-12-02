6ed94e89984c4e465276aa2741ff83d9eebf2623
aminuzainab <aminumikailzainab@gmail.com>
2025-12-02 13:20:39 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        id: 1,
+        mainImage: "./images/image-product-1.jpg",
+        thumbnail: "./images/image-product-1-thumbnail.jpg",
+        title: "Fall Limited Edition Sneakers",
+        price: 125,
+    },
+
+    {
+        id: 2,
+        mainImage: "./images/image-product-2.jpg",
+        thumbnail: "./images/image-product-2-thumbnail.jpg"
+    },
+
+    {
+        id: 3,
+        mainImage: "./images/image-product-3.jpg",
+        thumbnail: "./images/image-product-3-thumbnail.jpg"
+    },
+
+    {
+        id: 4,
+        mainImage: "./images/image-product-4.jpg",
+        thumbnail: "./images/image-product-4-thumbnail.jpg"
+    }
+]
